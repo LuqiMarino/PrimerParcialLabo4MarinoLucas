4c0a70998baf9c345f2d914e9371e0aeda72df74
LuqiMarino <lucasmarino96@hotmail.com>
2021-10-07 18:38:15 -0300

    asd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp } from "firebase/app";
+
+const app = initializeApp(environment.firebaseConfig);
 
 @NgModule({
   declarations: [
